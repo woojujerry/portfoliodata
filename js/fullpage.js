c2912d50fcf952b125230f0fa49e77fea52d9287
woojujerry <monkey_100@naver.com>
2023-11-13 17:42:52 +0900

    first commit

---
+++ js/fullpage.js
@@ -0,0 +1,16 @@
+$(function () {
+  new fullpage("#fullpage", {
+    verticalCentered: false,
+    scrollBar: true,
+    anchors: [
+      "anchor1",
+      "anchor2",
+      "anchor3",
+      "anchor4",
+      "anchor5",
+      "anchor6",
+      "anchor7"
+    ],
+    menu: "#menu"
+  });
+});
